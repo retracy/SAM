c8ab93a222a3c4eefe0ec0465b7da7fef0d19e50
Bob Tracy <retracy@live.com>
2018-08-18 18:36:06 -0700

    Implement a call with a struct argument

--- Wrapper/Program.cs
+++ Wrapper/Program.cs
@@ -7,10 +7,26 @@ namespace Wrapper
     {
         static void Main(string[] args)
         {
-            System.Diagnostics.Debug.WriteLine(fnDriver());
+            var info = new DeviceInfo
+            {
+                Version = 1
+            };
+
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(info));
+            Marshal.StructureToPtr(info, ptr, true);
+
+            GetDeviceInfo(ptr);
+
+            info = (DeviceInfo)Marshal.PtrToStructure(ptr, typeof(DeviceInfo));
+            System.Diagnostics.Debug.WriteLine(info.Version);
         }
 
-        [DllImport("Driver.dll")]
-        extern static private int fnDriver();
+        [DllImport("Driver.dll", CallingConvention = CallingConvention.Cdecl)]
+        extern static private void GetDeviceInfo(IntPtr pinfo);
+
+        public struct DeviceInfo
+        {
+            public uint Version;
+        }
     }
 }
